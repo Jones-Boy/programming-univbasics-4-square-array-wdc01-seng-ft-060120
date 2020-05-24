c1a02458119f906e8ec78580003cce8465a7f5fe
Michael Jones <djyoungjones@gmail.com>
2020-05-24 23:17:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,14 @@
 def square_array(array)
- new_numbers = numbers.collect{|num| num ** 2}
-  return new_numbers
+ new_array = []
+  index = 0
+ 
+  while index < array.length do
+    new_array << array[index] * array[index]
+    index += 1
+  end
+ 
+  new_array
+end
+
    
-end 
+
